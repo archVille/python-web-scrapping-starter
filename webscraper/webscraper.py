dbfcaa30ad6c4a96bafdd84072ea22a8de18d0ed
Panos Meretis <meretis.panagiotis@ground-control.co.uk>
2022-03-24 19:16:50 +0000

    adding webscrapper mechanism and rendering to the html

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -1,5 +1,19 @@
+from urllib import request
 from flask import Flask, render_template
+from bs4 import BeautifulSoup  
+import requests 
 app=Flask(__name__)
+
+source=requests.get('http://quotes.toscrape.com').text
+soup=BeautifulSoup(source, 'lxml')
+
+quote=soup.find('div', class_='quote')
+quotetext=quote.span.text
+print(quotetext)
+
+author=soup.find('small', class_='author')
+authortext=author.text
+print(authortext)
 
 app.route('/')
 def home():
